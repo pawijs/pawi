6cb2e1e32e189503ecb7a9da0f5d08fd448ee868
Maia Taewana <maia@pawijs.org>
2021-03-11 14:04:39 +1300

    fix(vscode): preparing for first automated release with repo-cooker

--- setup/repo-cooker.js
+++ setup/repo-cooker.js
@@ -2,20 +2,13 @@ import { Cooker } from 'repo-cooker'
 
 process.env.REPO_COOKER_GITHUB_TOKEN = process.env.GH_TOKEN
 
-const releaseGlobs = ['packages/pawi', 'packages/@pawi/*', 'other/*']
-
 export const cooker = Cooker(process.argv, {
-  devtools: {
-    host: 'localhost:8787, reconnect: false',
-  },
   path: '.',
-  packagesGlobs:
-    process.argv[2] === '--release'
-      ? releaseGlobs
-      : [
-          ...releaseGlobs,
-          'packages/examples',
-          'packages/vscode-view',
-          'packages/vscode-pawi',
-        ],
+  packagesGlobs: [
+    'other/*',
+    'packages/pawi',
+    'packages/@pawi/*',
+    'packages/examples',
+    'packages/vscode-pawi',
+  ],
 })
